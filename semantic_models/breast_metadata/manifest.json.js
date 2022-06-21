eaebe403ad33b3a11d7186c80ee18d1f444864d8
Andra Waagmeester <andra@micelio.be>
2022-06-21 15:58:36 +0200

    manifest for ShEx forms, plus minor typo fixes in obo schema

---
+++ semantic_models/breast_metadata/manifest.json.js
@@ -0,0 +1,12 @@
+[
+{
+"title": "Breast-cancer metadata based on OBO",
+"desc": "First draft of a schema to describe the metadata to drive data collection in the Delta-tissue Leap project",
+"schemaURL": "https://raw.githubusercontent.com/German-BioImaging/dtplatform/main/semantic_models/breast_metadata/obo_based_model.shex",
+"schemaFormat": "ShExC",
+"layoutURL": "empty.ttl",
+"dataURL": "profile1.ttl",
+"node": "patient:id",
+"shape": "#UserProfile"
+}
+]
